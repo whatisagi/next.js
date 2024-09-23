55245b53092d857cd81e841d46e5f929dcc4116a
Tobias Koppers <tobias.koppers@googlemail.com>
2024-09-23 09:15:07 +0200

    trace aggregation update

--- turbopack/crates/turbo-tasks-backend/src/backend/operation/aggregation_update.rs
+++ turbopack/crates/turbo-tasks-backend/src/backend/operation/aggregation_update.rs
@@ -228,12 +228,14 @@ impl AggregatedDataUpdate {
 #[derive(Default, Serialize, Deserialize, Clone)]
 pub struct AggregationUpdateQueue {
     jobs: VecDeque<AggregationUpdateJob>,
+    pub processed_jobs: usize,
 }
 
 impl AggregationUpdateQueue {
     pub fn new() -> Self {
         Self {
             jobs: VecDeque::with_capacity(8),
+            processed_jobs: 0,
         }
     }
 
@@ -256,6 +258,7 @@ impl AggregationUpdateQueue {
     }
 
     pub fn process(&mut self, ctx: &mut ExecuteContext<'_>) -> bool {
+        self.processed_jobs += 1;
         if let Some(job) = self.jobs.pop_front() {
             match job {
                 AggregationUpdateJob::UpdateAggregationNumber {

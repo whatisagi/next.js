0cf17e2eb278c937bc2cf90103545c289a27d866
Jiachi Liu <inbox@huozhi.im>
2024-09-24 13:05:39 +0200

    add missing pieces

--- packages/next/src/client/components/globals/intercept-console-error.ts
+++ packages/next/src/client/components/globals/intercept-console-error.ts
@@ -1,8 +1,12 @@
 import { isNextRouterError } from '../is-next-router-error'
 import { handleClientError } from '../react-dev-overlay/internal/helpers/use-error-handler'
 
+const originConsoleError = window.console.error
+
+const isReactOwnerStackEnabled = !!process.env.__NEXT_REACT_OWNER_STACK
+
 // Patch console.error to collect information about hydration errors
-export function patchConsoleError() {
+function patchConsoleError() {
   // Ensure it's only patched once
   if (typeof window === 'undefined') {
     return
@@ -11,7 +15,13 @@ export function patchConsoleError() {
   const originConsoleError = window.console.error
   window.console.error = (...args) => {
     // See https://github.com/facebook/react/blob/d50323eb845c5fde0d720cae888bf35dedd05506/packages/react-reconciler/src/ReactFiberErrorLogger.js#L78
-    const error = process.env.NODE_ENV !== 'production' ? args[1] : args[0]
+    const error =
+      process.env.NODE_ENV !== 'production'
+        ? isReactOwnerStackEnabled
+          ? args[1] || args[0]
+          : args[1]
+        : args[0]
+
 
     if (!isNextRouterError(error)) {
       if (process.env.NODE_ENV !== 'production') {
@@ -26,3 +36,5 @@ export function patchConsoleError() {
     }
   }
 }
+
+export { patchConsoleError, originConsoleError }

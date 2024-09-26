6ecdf594d3548ea89ea033bb6c303963b25f9255
Tobias Koppers <tobias.koppers@googlemail.com>
2024-09-26 15:29:25 +0200

    correct tracing span handling

--- turbopack/crates/turbo-tasks-fs/src/lib.rs
+++ turbopack/crates/turbo-tasks-fs/src/lib.rs
@@ -272,7 +272,8 @@ impl DiskFileSystem {
     }
 
     pub fn invalidate(&self) {
-        let span = tracing::info_span!("invalidate filesystem", path = &*self.root).entered();
+        let _span = tracing::info_span!("invalidate filesystem", path = &*self.root).entered();
+        let span = tracing::Span::current();
         let invalidator_map = take(&mut *self.invalidator_map.lock().unwrap());
         let dir_invalidator_map = take(&mut *self.dir_invalidator_map.lock().unwrap());
         let iter = invalidator_map
@@ -287,7 +288,8 @@ impl DiskFileSystem {
     }
 
     pub fn invalidate_with_reason(&self) {
-        let span = tracing::info_span!("invalidate filesystem", path = &*self.root).entered();
+        let _span = tracing::info_span!("invalidate filesystem", path = &*self.root).entered();
+        let span = tracing::Span::current();
         let invalidator_map = take(&mut *self.invalidator_map.lock().unwrap());
         let dir_invalidator_map = take(&mut *self.dir_invalidator_map.lock().unwrap());
         let iter = invalidator_map
